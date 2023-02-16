be15dc25383112ad7cf7b98eced7480db3a3d5f7
Vitor Adriano <112006474+Rybaksz@users.noreply.github.com>
2023-02-16 11:26:26 -0300

    Add files via upload

---
+++ Create Empresa.sql
@@ -0,0 +1,9 @@
+use GenericTrue
+
+
+Create table Empresa(
+	codigo int identity (1,1) primary key,
+	Nome varchar(100),
+	endereco varchar(100),
+	telefone varchar(25),
+	email varchar(100)	)
---
+++ Valores Empresa.sql
@@ -0,0 +1,10 @@
+INSERT INTO [dbo].[EMPRESA]
+           ([nome]
+           ,[endereco]
+           ,[telefone]
+           ,[email])
+     VALUES
+           ('GenericTrue',
+           'RUA XINGU, 333',
+           '3333-9999',
+           'Gnt@Gmail.com')
